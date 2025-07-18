36317e217cc29148dca8de57bedbaf6d72a3410f
kapilsarkar <k.sarkar81@gmail.com>
2025-07-18 15:17:20 +0530

    added

--- REACT-JS PROJECTS/E-COMMERCE-APP/src/components/Cart.jsx
+++ REACT-JS PROJECTS/E-COMMERCE-APP/src/components/Cart.jsx
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
-import { removeItems, clearCart } from "../utils/cartSlice";
+import { removeItems, clearCart} from "../utils/cartSlice";
 import { toast } from "react-toastify";
 
 const Cart = () => {
