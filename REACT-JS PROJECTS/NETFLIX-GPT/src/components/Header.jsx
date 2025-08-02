9e6d58219742a012920f2d46807ba67436e52020
kapilsarkar <k.sarkar81@gmail.com>
2025-08-02 13:23:08 +0530

    somepartsadded

--- REACT-JS PROJECTS/NETFLIX-GPT/src/components/Header.jsx
+++ REACT-JS PROJECTS/NETFLIX-GPT/src/components/Header.jsx
@@ -1,9 +1,33 @@
+/* eslint-disable no-unused-vars */
 import HeaderLogo from "../images/mainLogo.png";
+import netflixAvatar from "../images/netflix-avatar.jpg";
+import { signOut } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router";
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        navigate("/");
+      })
+      .catch((error) => {
+        // An error happened.
+        navigate("/error");
+      });
+  };
   return (
-    <div className="absolute w-screen px-7 py-2 bg-gradient-to-b from-black z-10">
-      <img className="w-44" src={HeaderLogo} alt="logo" />
+    <div className="absolute w-screen px-7 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
+      <img className="w-44  mx-auto md:mx-0" src={HeaderLogo} alt="logo" />
+      <div className=" flex p-2">
+        <img className="w-12 h-12 " alt="user-icon" src={netflixAvatar} />
+        <button onClick={handleSignOut} className=" font-bold text-white">
+          (Sign Out)
+        </button>
+      </div>
     </div>
   );
 };
