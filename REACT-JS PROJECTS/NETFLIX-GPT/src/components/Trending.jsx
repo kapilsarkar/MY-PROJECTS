9971ce7ca1893938a8e6ad73900de9ed440877c2
kapilsarkar <k.sarkar81@gmail.com>
2025-08-16 11:39:58 +0530

    somepartsadded

--- REACT-JS PROJECTS/NETFLIX-GPT/src/components/Trending.jsx
+++ REACT-JS PROJECTS/NETFLIX-GPT/src/components/Trending.jsx
@@ -9,7 +9,7 @@ const Trending = () => {
   return (
     <div className="w-full py-2 px-3 bg-black text-white">
       <h2 className="mt-2 text-3xl p-3 font-bold text-white">Trending Shows</h2>
-      <div className=" flex py-3 justify-center">
+      <div className=" flex rounded-xl py-3 justify-center border-t-8 border-red-500">
         <TrendingCard  trending={trending?.nowTrending} />
       </div>
     </div>
